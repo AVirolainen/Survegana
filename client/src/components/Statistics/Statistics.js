137dadb81cf5e8be744ee0d9e7a83baf85ba6c75
Stanislav <rudenkostas2@gmail.com>
2021-04-06 12:52:54 +0300

    front fixing

--- client/src/components/Statistics/Statistics.js
+++ client/src/components/Statistics/Statistics.js
@@ -1,8 +1,40 @@
-import React from "react"
+import React, {useState} from "react"
+import "./Statistics.css"
+import { TreeSelect } from 'antd';
 
 const Statistics = () =>{
+    const { TreeNode } = TreeSelect;
+
+    const [value, setValue] = useState(undefined);
+    const onChange = () => {
+        setValue(value);
+    };
+
     return(
-        <div>Statistics page</div>
+        <div className="statisticBody">
+            <div className="statisticHeader">Please, choose your survey</div>
+            <TreeSelect
+                showSearch
+                style={{ width: '100%' }}
+                value={value}
+                dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+                placeholder="Please select"
+                allowClear
+                treeDefaultExpandAll
+                onChange={onChange}
+            >
+                <TreeNode value="parent 1" title="parent 1">
+                    <TreeNode value="parent 1-0" title="parent 1-0">
+                        <TreeNode value="leaf1" title="leaf1" />
+                        <TreeNode value="leaf2" title="leaf2" />
+                    </TreeNode>
+                    <TreeNode value="parent 1-1" title="parent 1-1">
+                        <TreeNode value="leaf3" title={<b style={{ color: '#08c' }}>leaf3</b>} />
+                    </TreeNode>
+                </TreeNode>
+            </TreeSelect>
+        </div>
+
     )
 }
 
--- client/src/components/UserPage/UserPage.js
+++ client/src/components/UserPage/UserPage.js
@@ -1,8 +1,22 @@
 import React from "react"
+import "./UserPage.css"
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Table, Tag, Space } from 'antd';
 
 const UserPage = () =>{
+    const { Column, ColumnGroup } = Table;
+
+
     return(
-        <div>User page</div>
+        <div className="userPage">
+            <Avatar size={254} icon={<UserOutlined />} />
+            <div className="userName">User name</div>
+            <Table>
+                <Column title="Surveys passed" dataIndex="age" key="age" />
+                <Column title="Surveys created" dataIndex="address" key="address" />
+            </Table>
+        </div>
     )
 }
 
